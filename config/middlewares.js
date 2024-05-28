5df61560eadca09f497c677ba48a45899918b7e3
Mat Aldao <mataldao@Mats-MacBook-Pro.local>
2024-05-28 19:46:12 -0300

    Testing Spaces

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "strapi.io",
+            "sparring.s3.nyc3.amazonaws.com",
+            "https://sparring.s3.nyc3.amazonaws.com"
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "strapi.io",
+            "sparring.s3.nyc3.amazonaws.com",
+            "https://sparring.s3.nyc3.amazonaws.com"
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          credentials: {
+            accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+            secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+          },
+          region: env("DO_SPACE_REGION"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
+
+
