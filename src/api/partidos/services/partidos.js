200e298eb1d1333329804e6acf1782aba0958db7
Mat Aldao <mataldao@Mats-MacBook-Pro.local>
2024-10-01 17:36:23 -0300

    Pushing

--- src/api/partidos/services/partidos.js
+++ src/api/partidos/services/partidos.js
@@ -10,17 +10,14 @@ module.exports = {
               $null: true,
             },
           },
-          $and: [
-            {
-              // Exclude matches where the user is already a member or, if match owner, match their ID
+         
               $or: [
                 { member_1: { id: isMatchOwner ? userId : { $ne: userId } } },
                 { member_2: { id: isMatchOwner ? userId : { $ne: userId } } },
                 { member_3: { id: isMatchOwner ? userId : { $ne: userId } } },
                 { member_4: { id: isMatchOwner ? userId : { $ne: userId } } },
               ],
-            },
-          ],
+           
         },
         populate: {
             match_owner: { 
@@ -43,7 +40,6 @@ module.exports = {
       };
   
 
-      console.log('query', query.where.$and[0].$or);
       // Modify the query based on the isMatchOwner condition
     //   if (isMatchOwner) {
 
