442b0aaf09b8655bfff1455fdef86bc4d15e36fb
Mat Aldao <m@marcopolo.agency>
2025-04-23 22:47:45 -0300

    Pushing

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,22 +8,26 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "strapi.io",
-            "sparring.s3.nyc3.cdn.amazonaws.com",
-            "https://sparring.s3.nyc3.cdn.amazonaws.com"
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "strapi.io",
-            "sparring.s3.nyc3.cdn.amazonaws.com",
-            "https://sparring.s3.nyc3.cdn.amazonaws.com"
-          ],
+        "img-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "strapi.io",
+          "sparring.s3.nyc3.cdn.amazonaws.com",
+          "https://sparring.s3.nyc3.cdn.amazonaws.com",
+          "sparring.nyc3.digitaloceanspaces.com", 
+          "https://sparring.nyc3.digitaloceanspaces.com" 
+        ],
+        "media-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "strapi.io",
+          "sparring.s3.nyc3.cdn.amazonaws.com",
+          "https://sparring.s3.nyc3.cdn.amazonaws.com",
+          "sparring.nyc3.digitaloceanspaces.com", 
+          "https://sparring.nyc3.digitaloceanspaces.com"
+        ],
           upgradeInsecureRequests: null,
         },
       },
